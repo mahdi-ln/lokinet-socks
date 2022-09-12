f3e07d1afc8c18b8714786e8eb40ad61644768ee
mahdi-ln <mahg361@gmail.com>
2022-09-12 05:03:37 +0430

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-echo 'nameserver 127.3.2.1' > /etc/resolv.conf;./lokinet &
+echo 'nameserver 127.3.2.1' > /etc/resolv.conf;lokinet &
 
-ENV CFGFILE=/etc/dante/sockd.conf
-ENV PIDFILE=/run/sockd.pid
-ENV WORKERS=10
 
 while  [ ! -d "/sys/class/net/lokitun0" ]
+do
+:
 done
 sockd -f $CFGFILE -p $PIDFILE -N $WORKERS
